46a6e3882cf0ee3e69fabcff9de1ba47d1711101
antimon2 <antimon2.me@gmail.com>
2014-01-06 10:45:22 +0900

    transform.with_index が macruby で動作するよう修正。

--- lib/enumerable_lz/transform_mrb.rb
+++ lib/enumerable_lz/transform_mrb.rb
@@ -1,9 +1,11 @@
 # for MacRuby
 module Enumerable
   def transform &block
-    Transform.new self, &block
+    Enumerator::Transform.new self, &block
   end
+end
 
+class Enumerator
   class Transform < Enumerator
     def initialize obj, &transformer
       @org_enum = obj
@@ -36,6 +38,13 @@ module Enumerable
       cp.transform! &block
     end
 
+    #[override]
+    def with_index offset=0, &block
+      src_enum = @transformer.nil? || @transformer.size.zero? ? @org_enum : self
+      block ||= Proc.new{|el, i|[el, i]}
+      TransformWithIndex.new src_enum, offset, &block
+    end
+
     private
     def compile_transformer
       return Proc.new{|a|a} if @transformer.nil? || @transformer.size==0
@@ -51,5 +60,29 @@ module Enumerable
       #   eval "Proc.new{|el|"+codes+"}"
       # }.call(@transformer)
     end
+  end
+
+  # private
+  class TransformWithIndex < Transform
+    def initialize obj, offset = 0, &transformer
+      @org_enum = obj
+      @transformer = transformer.nil? ? nil : [transformer]
+      @offset = offset
+    end
+
+    def each
+      return self unless block_given?
+      the_transformer = @transformer[0] || Proc.new{|el,i|el}
+      i = @offset - 1
+      @org_enum.each do |el|
+        yield the_transformer.call(el, i+=1)
+      end
+    end
+
+    #[override]
+    def transform &block
+      Transform.new self, &block
+    end
+    alias :transform! :transform
   end
 end
